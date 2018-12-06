bae07399ab2ee8ee5b07005265285bcd3cb1b4d9
Morgan Edwards <medwardsii@gmail.com>
2018-12-06 14:43:35 -0500

    continues 6

--- day6.hs
+++ day6.hs
@@ -14,8 +14,19 @@ testInput = "1, 1\n\
 testOutput :: [Integer]
 testOutput = [(-1), (-1), (-1), 9, 17, (-1)]
 
+data Direction = Northwest Pos
+               | Northeast Pos
+               | Southeast Pos
+               | Southwest Pos
+               | North Pos
+               | East Pos
+               | South Pos
+               | West Pos
+               | Colocate Pos
+               deriving (Show)
+
 type Pos = (Int, Int)
-type Perimeter = (Maybe Pos, Maybe Pos, Maybe Pos, Maybe Pos)
+type Perimeter = ([Pos], [Pos], [Pos], [Pos])
 
 makeCoords :: [String] -> [Pos]
 makeCoords xs = map coordinate xs
@@ -23,27 +34,33 @@ makeCoords xs = map coordinate xs
     parse (x:y:_) = ((read . init $ x :: Int), (read y :: Int))
     coordinate x = (parse . words) x
 
-perimeter :: [Pos] -> Pos -> (Pos, Perimeter)
-perimeter allPos center = (center, (ne, nw, sw, se))
-  where
-    ne = Just center
-    nw = Just center
-    sw = Just center
-    se = Just center
+getDir :: Pos -> Pos -> Direction
+getDir (centerX, centerY) (tarX, tarY)
+  | tarY > centerY && tarX > centerX = Southeast (tarX, tarY)
+  | tarY > centerY && tarX < centerX = Southwest (tarX, tarY)
+  | tarY < centerY && tarX > centerX = Northeast (tarX, tarY)
+  | tarY < centerY && tarX < centerX = Northwest (tarX, tarY)
+  | tarY == centerY && tarX == centerX = Colocate (tarX, tarY)
+  | tarY == centerY && tarX > centerX = East (tarX, tarY)
+  | tarY == centerY && tarX < centerX = West (tarX, tarY)
+  | tarY > centerY && tarX == centerX = South (tarX, tarY)
+  | tarY < centerY && tarX == centerX = North (tarX, tarY)
 
-areaBetween :: Pos -> Maybe Pos -> Integer
-areaBetween x y = 3
+distanceBetween :: Pos -> Pos -> Int
+distanceBetween (x1, y1) (x2, y2) = distance
+   where
+    distance = abs (x1 - x2) + abs (y1 - y2)
 
-surroundingArea :: (Pos, Perimeter) -> Integer
-surroundingArea (center, (ne, nw, sw, se))
-  | not . null . filter (\x -> x == Nothing) $ [ne, nw, sw, se] = (-1)
-  | otherwise = sum $ map (areaBetween center) [ne, nw, sw, se]
+-- surroundingArea :: (Pos, Perimeter) -> Integer
+-- surroundingArea (center, (ne, nw, sw, se))
+--   | not . null . filter (\x -> x == Nothing) $ [ne, nw, sw, se] = (-1)
+--   | otherwise = sum $ map (areaBetween center) [ne, nw, sw, se]
 
 
-partOne :: [String] -> [Integer]
-partOne xs = map surroundingArea . map (perimeter coords) $ coords
-  where
-    coords = makeCoords xs
+-- partOne :: [String] -> [Integer]
+-- partOne xs = map surroundingArea . map (perimeter coords) $ coords
+--   where
+--     coords = makeCoords xs
 
 
 
@@ -52,5 +69,7 @@ main = do
   -- input <- readFile "./data/daySixData.txt"
   let input = testInput
   let clean = lines . init $ input
-  let solutionOne = partOne clean
-  print solutionOne
+  let allCoords = makeCoords clean
+  -- print allCoords
+  print $ map (distanceBetween (5, 5)) [(8, 9)]
+  -- print (map (getDir (allCoords !! 3)) allCoords)
