76217d0adb5ea891f1438be44ff404f97d78280f
Morgan Edwards <medwardsii@gmail.com>
2018-12-06 12:01:27 -0500

    sketches out solution

--- day6.hs
+++ day6.hs
@@ -3,14 +3,19 @@ module DaySix where
 import Data.List
 import Data.Char
 
-testInput :: [Pos]
-testInput = [(1,1), (1,6), (8,3), (3,4), (5,5), (8,9)]
+testInput :: String
+testInput = "1, 1\n\
+\1, 6\n\
+\8, 3\n\
+\3, 4\n\
+\5, 5\n\
+\8, 9\n"
 
 testOutput :: [Integer]
 testOutput = [(-1), (-1), (-1), 9, 17, (-1)]
 
 type Pos = (Int, Int)
-data Coord = Coord Char Pos
+type Perimeter = (Maybe Pos, Maybe Pos, Maybe Pos, Maybe Pos)
 
 makeCoords :: [String] -> [Pos]
 makeCoords xs = map coordinate xs
@@ -18,13 +23,34 @@ makeCoords xs = map coordinate xs
     parse (x:y:_) = ((read . init $ x :: Int), (read y :: Int))
     coordinate x = (parse . words) x
 
+perimeter :: [Pos] -> Pos -> (Pos, Perimeter)
+perimeter allPos center = (center, (ne, nw, sw, se))
+  where
+    ne = Just center
+    nw = Just center
+    sw = Just center
+    se = Just center
+
+areaBetween :: Pos -> Maybe Pos -> Integer
+areaBetween x y = 3
+
+surroundingArea :: (Pos, Perimeter) -> Integer
+surroundingArea (center, (ne, nw, sw, se))
+  | not . null . filter (\x -> x == Nothing) $ [ne, nw, sw, se] = (-1)
+  | otherwise = sum $ map (areaBetween center) [ne, nw, sw, se]
+
+
+partOne :: [String] -> [Integer]
+partOne xs = map surroundingArea . map (perimeter coords) $ coords
+  where
+    coords = makeCoords xs
 
 
 
 main :: IO ()
 main = do
   -- input <- readFile "./data/daySixData.txt"
-  -- let input = testInput
-  -- let clean = lines . init $ input
-  let solutionOne = testInput
+  let input = testInput
+  let clean = lines . init $ input
+  let solutionOne = partOne clean
   print solutionOne
