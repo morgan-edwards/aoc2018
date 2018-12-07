c07b26fae54d6cdef532308830413df799a00fa7
Morgan Edwards <morganedwards@Morgans-iMac.local>
2018-12-06 20:30:16 -0500

    gets interior points

--- day6.hs
+++ day6.hs
@@ -14,19 +14,10 @@ testInput = "1, 1\n\
 testOutput :: [Integer]
 testOutput = [(-1), (-1), (-1), 9, 17, (-1)]
 
-data Direction = Northwest Pos
-               | Northeast Pos
-               | Southeast Pos
-               | Southwest Pos
-               | North Pos
-               | East Pos
-               | South Pos
-               | West Pos
-               | Colocate Pos
-               deriving (Show)
+data Direction = North | East | South | West deriving (Show)
 
 type Pos = (Int, Int)
-type Perimeter = ([Pos], [Pos], [Pos], [Pos])
+type Perimeter = (Pos, Pos, Pos, Pos)
 
 makeCoords :: [String] -> [Pos]
 makeCoords xs = map coordinate xs
@@ -34,22 +25,39 @@ makeCoords xs = map coordinate xs
     parse (x:y:_) = ((read . init $ x :: Int), (read y :: Int))
     coordinate x = (parse . words) x
 
-getDir :: Pos -> Pos -> Direction
-getDir (centerX, centerY) (tarX, tarY)
-  | tarY > centerY && tarX > centerX = Southeast (tarX, tarY)
-  | tarY > centerY && tarX < centerX = Southwest (tarX, tarY)
-  | tarY < centerY && tarX > centerX = Northeast (tarX, tarY)
-  | tarY < centerY && tarX < centerX = Northwest (tarX, tarY)
-  | tarY == centerY && tarX == centerX = Colocate (tarX, tarY)
-  | tarY == centerY && tarX > centerX = East (tarX, tarY)
-  | tarY == centerY && tarX < centerX = West (tarX, tarY)
-  | tarY > centerY && tarX == centerX = South (tarX, tarY)
-  | tarY < centerY && tarX == centerX = North (tarX, tarY)
+directionSort :: Direction -> Pos -> Pos -> Ordering
+directionSort d (x1, y1) (x2, y2)
+  | c1 > c2 = GT
+  | c1 < c2 = LT
+  | c1 == c2 = EQ
+  where
+    (c1, c2) = case d of
+      North -> (y1, y2)
+      South -> (y2, y1)
+      West -> (x1, x2)
+      East -> (x2, x1)
+
+furthest :: Direction -> [Pos] -> (Pos, Pos)
+furthest d ps = (head sorted, last sorted)
+  where
+    sorted = sortBy (directionSort d) ps
+
+perimeter :: [Pos] -> Perimeter
+perimeter ps = (fst ns, snd we, snd ns, fst we)
+  where
+    ns = furthest North ps
+    we = furthest West ps
 
 distanceBetween :: Pos -> Pos -> Int
 distanceBetween (x1, y1) (x2, y2) = distance
    where
     distance = abs (x1 - x2) + abs (y1 - y2)
+
+-- interiorPoints :: Perimeter -> (Int, Int, Int, Int)
+-- interiorPoints ((_, n), (e, _), (_, s), (w, _)) = (n, e, s, w)
+interiorPoints :: Perimeter -> [Pos] -> [Pos]
+interiorPoints ((_, n), (e, _), (_, s), (w, _)) ps = [ p | p <- ps, fst p < e && fst p > w && snd p > n && snd p < s]
+
 
 -- surroundingArea :: (Pos, Perimeter) -> Integer
 -- surroundingArea (center, (ne, nw, sw, se))
@@ -66,10 +74,12 @@ distanceBetween (x1, y1) (x2, y2) = distance
 
 main :: IO ()
 main = do
-  -- input <- readFile "./data/daySixData.txt"
-  let input = testInput
+  input <- readFile "./data/daySixData.txt"
+  -- let input = testInput
   let clean = lines . init $ input
   let allCoords = makeCoords clean
-  -- print allCoords
-  print $ map (distanceBetween (5, 5)) [(8, 9)]
+  let outer = perimeter allCoords
+  let finitePoints = interiorPoints outer allCoords
+  print outer
+  print . length $ finitePoints
   -- print (map (getDir (allCoords !! 3)) allCoords)
