7695735363ea2f32721590f70ad44901ad395160
Morgan Edwards <morganedwards@Morgans-iMac.local>
2018-12-10 08:36:01 -0500

    continues exploring Haskell

--- day7.hs
+++ day7.hs
@@ -14,19 +14,47 @@ testInput = "Step C must be finished before step A can begin.\n\
 
 type Step = Char
 type Prereqs = [Char]
-data Procedure = Procedure Step Prereqs deriving (Show)
+data Procedure = Procedure { step :: Step,
+                             preqs :: Prereqs
+                           } deriving (Show)
 
 processSteps :: [String] -> [(Step, Step)]
-processSteps (x:xs)
-  | null xs = []
-  | otherwise = getSteps x : processSteps xs
+processSteps xs = map wordsToSteps xs
   where
-    getSteps x = let ws = words x in (head (ws !! 1), head (ws !! 7))
+    wordsToSteps x = let ws = words x in (head (ws !! 1), head (ws !! 7))
 
-makeProcedures :: [(Step, Step)] -> [Procedure]
-makeProcedures xs = undefined
+makeProcs :: [(Step, Step)] -> [Procedure]
+makeProcs xs = foldl uniq [] $ map stepToProc xs
+  where
+    prereqs step = map fst $ filter (\x-> snd x == step) xs
+    stepToProc x = let step = snd x in Procedure step (prereqs step)
+    uniq acc (Procedure s prs)
+      | elem s (map (step) acc) = acc
+      | otherwise = (Procedure s prs):acc
 
-testSolutionOne :: String
+allSteps :: [(Step, Step)] -> [Step]
+allSteps xs = foldl aggSts [] xs
+  where
+    aggSts acc x
+      | not (elem (fst x) acc) = fst x:acc
+      | not (elem (snd x) acc) = snd x:acc
+      | otherwise = acc
+
+firstStep :: [Step] -> [Procedure] -> Step
+firstStep ss ps = head . sort $ filter (not . hasPreReq ps) ss
+  where
+    hasPreReq ps s = elem s $ map (step) ps
+
+possibleSteps :: [Step] -> [Procedure] -> [Step]
+possibleSteps done remain = sort $ map (\(Procedure s prqs) -> s) $ filter (\x -> null . remainingPres $ x) remain
+  where
+    remainingPres (Procedure s prqs) = filter (\x -> not $ elem x done) prqs
+
+-- get first step
+-- get next step
+-- remove the proc from the remaining list
+-- when no procs remain, you're done
+
 testSolutionOne = "CABDFE"
 
 solutionOne :: [String] -> String
@@ -37,5 +65,11 @@ main = do
   -- input <- readFile "./data/daySevenData.txt"
   let input = testInput
   let clean = lines . init $ input
-  print . makeProcedures . processSteps $ clean
+  let stages = processSteps clean
+  let procs = makeProcs stages
+  let steps = allSteps stages
+  let done = "CA"
+  let next = possibleSteps done procs
+  print procs
+  print next
   -- print $ solutionOne clean == testSolutionOne
